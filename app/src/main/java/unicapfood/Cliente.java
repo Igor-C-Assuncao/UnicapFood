b251bd08886ba4c693a3139c2d5e3cc9d0413bdc
IGOR CASSIMIRO ASSUNCAO <89754060+Igor-C-Assuncao@users.noreply.github.com>
2022-09-09 01:35:29 -0300

    clientes iniciando

---
+++ app/src/main/java/unicapfood/Cliente.java
@@ -0,0 +1,22 @@
+package unicapfood;
+
+public class Cliente {
+
+    private String nome;
+    private int[] cpf;
+    private String senha;
+    
+    
+    public Cliente(String nome, int[] cpf, String senha) {
+        
+        
+        this.nome = nome;
+        this.cpf = new int[11];
+        this.senha = senha;
+    }
+
+    
+        }
+
+
+
