1e7c93b4ae14e99149484afb5793303a21b4f9ba
andreltlc <andrecarvalho1402@gmail.com>
2022-09-19 20:57:51 -0300

    Atribuição da comanda

--- app/src/main/java/unicapfood/Comanda.java
+++ app/src/main/java/unicapfood/Comanda.java
@@ -1,5 +1,12 @@
 package unicapfood;
 
 public class Comanda {
+    private Cliente cliente;
+    private int nComanda;
+    public Comanda(Cliente cliente, int nComanda) {
+        this.cliente = cliente;
+        this.nComanda = nComanda;
+    }
     
+
 }
