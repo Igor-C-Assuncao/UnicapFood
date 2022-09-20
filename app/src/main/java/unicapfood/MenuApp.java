f74cb2514a6b634c2f24177e435356972587ca5d
tolbarroso <carol.00000829955@unicap.br>
2022-09-19 21:24:39 -0300

    alteração menu

--- app/src/main/java/unicapfood/MenuApp.java
+++ app/src/main/java/unicapfood/MenuApp.java
@@ -12,11 +12,27 @@ public class MenuApp {
             exibeMenu();
             op = in.nextInt(); in.nextLine();
             switch (op) {
-                case 1: 
+                case 1: System.out.println("");
                         break;
-                case 2: 
+                case 2: System.out.println("");
                         break;
-                case 0: System.out.println("Programa encerrado!");
+                case 3: System.out.println("");
+                        break;
+                case 4: System.out.println("");
+                        break;
+                case 5: System.out.println("");
+                        break;
+                case 6: System.out.println("");
+                        break;
+                case 7: System.out.println("");
+                        break;
+                case 8: System.out.println("");
+                        break;
+                case 9: System.out.println("");
+                        break;
+                case 10: System.out.println("");
+                        break;
+                case 0: System.out.println("Tchau! Obrigada por utilizar nosso app!!");
                         break;
                 default: System.out.println("Opção inválida");
             }           
@@ -26,5 +42,14 @@ public class MenuApp {
         System.out.println("Opções:");
         System.out.println("1 - ");
         System.out.println("2 - ");
+        System.out.println("3 - ");
+        System.out.println("4 - ");
+        System.out.println("5 - ");
+        System.out.println("6 - ");
+        System.out.println("7 - ");
+        System.out.println("8 - ");
+        System.out.println("9 - ");
+        System.out.println("10 - ");
+        System.out.println("0 - Sair");
     }
 }
