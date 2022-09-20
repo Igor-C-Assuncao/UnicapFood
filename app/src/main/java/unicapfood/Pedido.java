eb96367af285ef67770f2cbde76eab65f3108a28
tolbarroso <carol.00000829955@unicap.br>
2022-09-19 22:00:27 -0300

    alterações

--- app/src/main/java/unicapfood/Pedido.java
+++ app/src/main/java/unicapfood/Pedido.java
@@ -1,5 +1,23 @@
 package unicapfood;
 
-public class Pedido {
+public class Pedido implements Comparable <Pedido>{
+    private String item;
+
+    public Pedido(String item) {
+        this.item = item;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
     
+    public int compareTo (Pedido pedido) {
+        int result;
+        result = this.getItem().compareTo(pedido.getItem());
+        return result;
+    }
 }
