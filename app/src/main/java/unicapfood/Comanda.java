fa4d7147b5cd651aa8c3f59b119499cc03a03b2f
tolbarroso <carol.00000829955@unicap.br>
2022-09-19 21:10:57 -0300

    toc

--- app/src/main/java/unicapfood/Comanda.java
+++ app/src/main/java/unicapfood/Comanda.java
@@ -3,10 +3,10 @@ package unicapfood;
 public class Comanda {
     private Cliente cliente;
     private int nComanda;
+
     public Comanda(Cliente cliente, int nComanda) {
         this.cliente = cliente;
         this.nComanda = nComanda;
     }
     
-
 }
