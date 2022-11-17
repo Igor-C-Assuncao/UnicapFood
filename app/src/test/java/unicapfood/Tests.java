9eedf384120c5fc1ebd0124dce1b061351ffb99c
IGOR CASSIMIRO ASSUNCAO <89754060+Igor-C-Assuncao@users.noreply.github.com>
2022-11-17 10:38:43 -0300

    adicionando testes

---
+++ app/src/test/java/unicapfood/Tests.java
@@ -0,0 +1,30 @@
+package unicapfood;
+
+import unicapfood.Negocio.Clientes.Cliente;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class Tests {
+
+    private Cliente cliente;
+
+
+    @BeforeEach
+    public void create(){
+        this.cliente = new Cliente( "carol", 123456, "1234");
+
+        
+    }
+
+    @Test
+    public void addCliente(){
+
+        
+       
+        
+    }
+    
+}
